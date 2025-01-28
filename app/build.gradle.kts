cbd112e3485d903783529cff379fd0425dba785b
Aizat <aliya.2005kaizat@gmail.com>
2025-01-29 01:57:06 +0300

    sdk 34 -> 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,12 +9,12 @@ plugins {
 
 android {
     namespace = "com.example.twix"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.twix"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
